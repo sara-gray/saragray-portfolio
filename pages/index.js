b84fa689076fa9a9db91eb8cef4f2f378036d67e
Sara Gray <saragrayen@gmail.com>
2022-04-12 22:30:10 +0100

    extracted component from index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
-import Image from 'next/image'
-
 import { getAllPostsForHome } from '@/lib/api'
 import Layout from '@/components/Layout'
+import Landing from '@/components/Landing'
 
 export default function Home({ allPosts }) {
 	const getBlogList = (posts) => {
@@ -14,27 +13,7 @@ export default function Home({ allPosts }) {
 
 	return (
 		<Layout>
-			<main className='absolute inset-0 bg-gray-900 w-screen h-[80%]'>
-				<h3 className='relative mt-12 ml-12 text-white text-4xl'>sara.gray</h3>
-				<div className='absolute top-0 right-0 w-[60%] h-full'>
-					<div className='relative w-full h-full'>
-						<Image
-							src='/assets/logo.png'
-							alt='sara gray logo'
-							layout='fill'
-							objectFit='cover'
-						/>
-					</div>
-				</div>
-				<div className='absolute bottom-[20%] right-[1%] text-white uppercase text-xs transform rotate-90'>
-					simply built and enjoyed
-				</div>
-				<p className='absolute z-10 bottom-14 bg-logo-kharki w-32 h-8'></p>
-				<h1 className='relative z-20 mt-24 ml-12 w-[30%] h-full text-gray-500 text-2xl'>
-					Dedicated, intuitive, somewhat determined{' '}
-					<spa className='text-gray-900'>expert</spa>
-				</h1>
-			</main>
+			<Landing />
 			<section>
 				<h3 className='text-black text-4xl'>sara.gray</h3>
 			</section>
