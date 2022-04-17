298dd721eb82432929cc435815c00747aecbc63b
Sara Gray <saragrayen@gmail.com>
2022-04-17 21:45:49 +0100

    Update Landing.js

--- components/Landing.js
+++ components/Landing.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const Landing = () => {
 	return (
-		<main className='bg-white w-screen h-screen'>
+		<main className='bg-white w-screen h-screen overflow-hidden'>
 			<section className='absolute inset-0 bg-gray-900 w-screen h-[30%] md:h-[80%] z-0'>
 				<h3 className='absolute top-[10%] left-[10%] text-white text-2xl md:text-4xl z-10'>
 					saragray.dev
@@ -25,7 +25,7 @@ const Landing = () => {
 				</div>
 				<p className='absolute bottom-[20%] left-0 bg-primary-plum w-[18%] h-7 block z-`0'></p>
 			</section>
-			<h3 className='absolute bottom-[5%] left-[10%] text-gray-900 font-semibold text-2xl md:text-4xl z-10'>
+			<h3 className='absolute bottom-[65%] md:bottom-[5%] left-[10%] text-black font-semibold text-2xl md:text-4xl'>
 				saragray.dev
 			</h3>
 		</main>
