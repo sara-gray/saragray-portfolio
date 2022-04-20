9c750e367047add21e267a242319b6fefbd5b2d3
Sara Gray <saragrayen@gmail.com>
2022-04-20 21:49:09 +0100

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -3,17 +3,23 @@ import Image from 'next/image'
 const Hero = () => {
 	return (
 		<main className=''>
-			<section className='text-primary-plum font-semibold capitalize  '>
-				<p>simply built and enjoyed</p>
-				<p>Trustworthy and reliable code</p>
+			<section className='mx-6 md:mx-12 lg:mx-24 py-8 text-primary-plum'>
+				<p>trustworthy reliable code</p>
 			</section>
-			{/* <section className='relative w-screen h-full mt-12 z-0 ring-2'>
+			<section className='relative w-screen h-56'>
 				<div className='relative w-full h-full'>
-					<Image src='/assets/laptop.jpg' alt='' width={2508} height={1672} />
+					<Image
+						src='/assets/laptop.jpg'
+						alt=''
+						layout='fill'
+						objectFit='cover'
+					/>
 				</div>
 				<div className='absolute inset-0 w-screen h-full bg-black/60'></div>
-			</section> */}
-			{/* <h3 className='text-black text-2xl md:text-4xl'>say something else</h3> */}
+			</section>
+			<h3 className='mx-6 md:mx-12 lg:mx-24 py-8 text-stone-800'>
+				simply built and enjoyed
+			</h3>
 		</main>
 	)
 }
