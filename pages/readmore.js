3131f68263f705b2426cc98485f6697b34466085
Sara Gray <saragrayen@gmail.com>
2022-04-23 21:47:49 +0100

    Update readmore.js

--- pages/readmore.js
+++ pages/readmore.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const readmore = () => {
-	return <div>readmore</div>
+	return (
+		<main className='bg-stone-900 w-screen h-screen'>
+			<Layout title={'About our work'}>
+				<h1>Coming soon...</h1>
+				<Footer />
+			</Layout>
+		</main>
+	)
 }
 
 export default readmore
