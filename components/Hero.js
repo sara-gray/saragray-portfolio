5332ac3d0e14733dd5ea9dfa5b5f0468f17bcffa
Sara Gray <saragrayen@gmail.com>
2022-04-30 08:29:30 +0100

    Added css for xl screens

--- components/Hero.js
+++ components/Hero.js
@@ -3,10 +3,10 @@ import Image from 'next/image'
 const Hero = () => {
 	return (
 		<main className=''>
-			<section className='mx-6 md:mx-12 lg:mx-24 py-8 text-primary-plum'>
-				<p className='text-2xl'>trustworthy reliable code</p>
+			<section className='mx-6 md:mx-12 lg:mx-24 xl:mx-48 py-8 text-primary-plum'>
+				<p className='text-2xl xl:text-4xl'>trustworthy reliable code</p>
 			</section>
-			<section className='relative w-screen h-56 lg:h-[80vh]'>
+			<section className='relative w-screen h-56 lg:h-[70vh]'>
 				<div className='relative w-full h-full'>
 					<Image
 						src='/assets/laptop.jpg'
@@ -17,7 +17,7 @@ const Hero = () => {
 				</div>
 				<div className='absolute inset-0 w-screen h-full bg-black/60'></div>
 			</section>
-			<h3 className='mx-6 md:mx-12 lg:mx-24 py-8 text-stone-800 text-2xl'>
+			<h3 className='mx-6 md:mx-12 lg:mx-24 xl:mx-48 py-8 text-stone-800 text-2xl xl:text-4xl'>
 				simply built and enjoyed
 			</h3>
 		</main>
--- components/Landing.js
+++ components/Landing.js
@@ -5,13 +5,13 @@ const Landing = () => {
 		<main className='relative inset-0 bg-white'>
 			<section className='bg-gray-900 w-full flex flex-row '>
 				<div className='w-1/2 flex-none'>
-					<h3 className='mx-6 md:mx-12 lg:mx-24 pt-12 text-white font-semibold text-2xl md:text-4xl'>
+					<h3 className='mx-6 md:mx-12 lg:mx-24 xl:mx-48 pt-12 xl:pt-24 text-white font-semibold text-2xl md:text-4xl xl:text-7xl'>
 						saragray.dev
 					</h3>
-					<h1 className='mx-6 md:mx-12 lg:mx-24 py-12 md:pt-30 lg:pt-40 xl:pt-56 w-[25%] text-gray-500 text-2xl'>
+					<h1 className='mx-6 md:mx-12 lg:mx-24 xl:mx-48 py-12 md:pt-30 lg:pt-40 xl:pt-72 w-[25%] text-gray-500 text-2xl xl:text-5xl'>
 						Dedicated, intuitive, <span className='text-white'>expert</span>
 					</h1>
-					<p className='relative bottom-[15%] lg:bottom-[18%] z-10 bg-primary-plum/50 w-[50%] h-7 block'></p>
+					<p className='relative bottom-[15%] lg:bottom-[18%] xl:bottom-[14%] z-10 bg-primary-plum/50 w-[50%] h-7 xl:h-12 block'></p>
 				</div>
 				<div className='relative w-full flex-1'>
 					<Image
@@ -20,12 +20,12 @@ const Landing = () => {
 						layout='fill'
 						objectFit='cover'
 					/>
-					<div className='absolute bottom-4 right-8 md:right-12 text-white uppercase text-[0.6rem] origin-bottom-right rotate-90'>
+					<div className='absolute bottom-4 right-8 md:right-12 xl:mr-24 text-white uppercase text-[0.6rem] xl:text-[1rem] origin-bottom-right rotate-90'>
 						simply built and enjoyed
 					</div>
 				</div>
 			</section>
-			<h3 className='mx-6 md:mx-12 lg:mx-24 py-8 text-stone-800 font-semibold text-2xl md:text-4xl'>
+			<h3 className='py-8 text-stone-800 font-semibold mx-6 md:mx-12 lg:mx-24 xl:mx-48 pt-12 xl:pt-24  text-2xl md:text-4xl xl:text-7xl'>
 				saragray.dev
 			</h3>
 		</main>
